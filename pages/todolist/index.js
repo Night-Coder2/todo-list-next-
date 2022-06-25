36bbb6fbf28d152e0e941944acf265063ebe817e
Sudhir <sudhircks@gmail.com>
2022-06-25 18:14:06 +0530

    j

--- pages/todolist/index.js
+++ pages/todolist/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import styles from '../../styles/Todolist.module.css'
 import TodoList from '../../react-scripts/TodoList/TodoList'
 
 const localStorageKey = 'todos'
@@ -49,19 +50,19 @@ const App = () => {
   }
 
   return (<>
-      <div className="bg-gray-900 text-gray-100 mx-auto text-center w-full h-full">
-        <h3 className="font-medium">Your TodoList</h3>
+      <div className={styles.div1}>
+        <h2>Your TodoList</h2>
         <br/>
         <form>
-        <input className="bg-gray-100 text-gray-900" ref={todoNameRef} type='text' />
-        <button className="bg-red-500 border-todo-4 border-gray-900" type='submit' onClick={handleAddTodo}>Add Todo</button>
-        <button className="bg-blue-400 border-todo-4 border-gray-900" onClick={handleClearTodos}>Clear Complete</button>
+        <input className={styles.input} ref={todoNameRef} type='text' />
+        <button className={styles.addTodo} type='submit' onClick={handleAddTodo}>Add Todo</button>
+        <button className={styles.clearComplete} onClick={handleClearTodos}>Clear Complete</button>
         </form>
         <div >{todos.filter(todo => !todo.complete).length} left to do</div>
         <br/>
         <div>
           <TodoList todos={todos} toggleTodo={toggleTodo}/>
-          <div className="text-cyan-500 underline"><a href='/'>return to home</a></div>
+          <div className={styles.aTag}><a href='/'>return to home</a></div>
         </div>
       </div>
     </>
